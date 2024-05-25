f962516e0cdcee64e771af999343635019cb8b66
Evgeny Bayandin <5087081@gmail.com>
2024-05-25 19:18:30 +0300

    Added GetMapping PostMapping endpoints

--- src/main/java/ru/webdev/controller/PersonController.java
+++ src/main/java/ru/webdev/controller/PersonController.java
@@ -1,19 +1,41 @@
 package ru.webdev.controller;
 
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ru.webdev.model.Person;
 import ru.webdev.repository.PersonRepository;
 
 @RestController
+@RequestMapping("/person")
 public class PersonController {
 
     @Autowired
     private PersonRepository personRepository;
 
     @GetMapping
-    public Iterable<Person> getPerson() {
+    public Iterable<Person> getPersons() {
         return personRepository.findAll();
+    }
+
+    @PostMapping("save")
+    public Person addPerson(@RequestBody Person person) {
+        return personRepository.save(person);
+    }
+
+    @GetMapping("{id}")
+    public Optional<Person> findById(@PathVariable int id) {
+        return personRepository.findById(id);
+    }
+
+    @GetMapping("find")
+    public Iterable<Person> findByNameIgnoreCase(@RequestParam String name) {
+        return personRepository.findPersonByNameContainingIgnoreCase(name);
     }
 }
--- src/main/java/ru/webdev/repository/PersonRepository.java
+++ src/main/java/ru/webdev/repository/PersonRepository.java
@@ -1,10 +1,12 @@
 package ru.webdev.repository;
 
+import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import ru.webdev.model.Person;
 
 @Repository
-public interface PersonRepository extends CrudRepository<Person, Long>{
+public interface PersonRepository extends CrudRepository<Person, Integer>{
 
+    Iterable<Person> findPersonByNameContainingIgnoreCase(String name);
 }
